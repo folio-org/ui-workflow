aaa4940b74f788840116de6295da9dd21de39ff4
Kevin Day <kday@tamu.edu>
2024-06-10 16:28:14 -0500

    UIWRKFLOW-7: Setup initial unit tests, basing off of documentation.
    Add several files based on documentation from the official websites.
    
    Add test for the failure case of `uploadMultipart()`.

--- src/hooks/useUpload/useUploadMultipart.test.ts
+++ src/hooks/useUpload/useUploadMultipart.test.ts
@@ -1,6 +1,20 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { Response, Server } from 'miragejs';
 
+import { OKAPI_WORKFLOW_IMPORT } from '../../constants';
+import { startMirage } from '../../../test/mirage';
 import { useUploadMultipart } from './useUploadMultipart';
+
+const path_prefix = 'workflow/';
+let server: Server;
+
+beforeEach(() => {
+  server = startMirage({});
+});
+
+afterEach(() => {
+  server.shutdown();
+});
 
 describe('useUploadMultipart', () => {
   describe('When initial render happened', () => {
@@ -8,6 +22,26 @@ describe('useUploadMultipart', () => {
 
     it('is expected to return defined', () => {
       expect(result.current).toBeDefined();
+      expect(result.current.uploadMultipart).toBeDefined();
     });
   });
+
+  describe('When uploadMultipart() fails expectedly', () => {
+    const { result } = renderHook(() => useUploadMultipart());
+    let file = { name: 'file_name.txt' };
+    const onSuccessMock = jest.fn();
+    const onErrorMock = jest.fn();
+
+    it('is expected to fail', async () => {
+      server.post(path_prefix + OKAPI_WORKFLOW_IMPORT, () => new Response(404));
+
+      await act(() => {
+        result.current.uploadMultipart(OKAPI_WORKFLOW_IMPORT, file, onSuccessMock, onErrorMock);
+      });
+
+      expect(onErrorMock).toBeCalled();
+      expect(onSuccessMock).not.toBeCalled();
+    });
+  });
+
 });
