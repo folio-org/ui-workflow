e100820bf7d0906de8833bf84e72660a6a1a0650
Kevin Day <kday@tamu.edu>
2024-08-07 16:18:55 -0500

    Switch from FormattedMessage to t() in ItemRecordGeneralPane.

--- src/components/ItemRecordGeneralPane/ItemRecordGeneralPane.tsx
+++ src/components/ItemRecordGeneralPane/ItemRecordGeneralPane.tsx
@@ -3,12 +3,12 @@ import { FormattedMessage } from 'react-intl';
 import { ErrorBoundary, Pane } from '@folio/stripes/components';
 
 import { IItemRecordView } from '../../interfaces';
+import { t } from '../../utilities';
 
 export const ItemRecordGeneralPane: React.FC<IItemRecordView> = ({ itemRecordDetail, view, stripes }) => {
-  const paneTitle= <FormattedMessage id='ui-workflow.title.itemRecordGeneralPane' />
   const onClose = !!itemRecordDetail?.onClose ? itemRecordDetail.onClose : false;
 
-  return <Pane defaultWidth='fill' dismissible onClose={onClose} paneTitle={paneTitle}>
+  return <Pane defaultWidth='fill' dismissible onClose={onClose} paneTitle={ t('title.itemRecordGeneralPane') }>
     <ErrorBoundary>
     </ErrorBoundary>
   </Pane>;
