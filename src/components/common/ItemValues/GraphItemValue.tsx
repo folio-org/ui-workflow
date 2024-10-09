43e4c917b3c5d796b533849545f30cbfd8a2b356
Rajdeep Hundal <rhundal7@tamu.edu>
2024-10-09 13:27:43 -0500

    initail commit - wip

--- src/components/common/ItemValues/GraphItemValue.tsx
+++ src/components/common/ItemValues/GraphItemValue.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card } from '@folio/stripes/components';
 
 import { GraphItemIcon } from '../../../components';
@@ -15,22 +15,52 @@ export const GraphItemValue: React.FC<IItemValue> = ({ empty, onSelect, selected
     return null;
   }
 
-  const cardClass = !!selected?.id && value?.id === selected.id ? css?.selected : null;
+  const [isHovered, setIsHovered] = useState(false);
+  const [isclicked, setIsClicked] = useState(false);
+  // const cardClass = !!selected?.id && value?.id === selected.id ? css?.selected : null;
+
+  const cardClickedBorder = isclicked ? '2px solid black' : 'none';
+  const removeBorder = !isclicked ? css.unSelected : cardClickedBorder;
+  // const cardClassHover = isHovered : 'applyHover' : 'default';
+
+  const [removePrevBorder, setRemovePrevBorder] = useState(false);
 
   const onClick = (e: any) => {
     if (!!onSelect) onSelect(e, value);
+
   };
 
-  return <Card
+  return (
+    <Card
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    onMouseDown={() => setIsClicked(true)}
+    onMouseOut={() => {
+      //  setIsClicked(false);
+      // setRemovePrevBorder(true);
+    }}
+    onMouseIn={() => {
+
+      // if(removePrevBorder){
+      //   setIsClicked(true);
+      //   setRemovePrevBorder(false);
+      // }
+    }}
     headerClass={ css?.nodeHeader }
     bodyClass={ css?.nodeBody }
-    cardClass={cardClass}
+    cardClass={removeBorder}
+    cardStyle={isHovered || isclicked ? 'positive' : 'default'}
     headerEnd={ <span className={ css?.headerEnd }>{ value?.name }</span> }
     headerStart={ <GraphItemIcon type={ value?.deserializeAs } /> }
     key={ value?.id }
     onClick={onClick}
+    style={{
+      cursor: 'pointer',
+      border: cardClickedBorder
+    }}
     roundedBorder
   >
     { value?.description }
-  </Card>;
+  </Card>
+  );
 };
