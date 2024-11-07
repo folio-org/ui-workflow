287cdf8169ab96def44151ac3cd285d52f61a2bb
Kevin Day <kday@tamu.edu>
2024-11-07 09:14:42 -0600

    Use search as result.current to improve test readability.

--- src/hooks/network/useSearch/useSearch.test.ts
+++ src/hooks/network/useSearch/useSearch.test.ts
@@ -1,4 +1,5 @@
 import { renderHook } from '@testing-library/react';
+import { SearchField } from '@folio/stripes/components';
 
 import { ITEM_COLUMNS_NAME, VIEW } from '../../../constants';
 import { useSearch } from '.';
@@ -6,9 +7,14 @@ import { useSearch } from '.';
 describe('useSearch', () => {
   describe('When initial render happened', () => {
     const { result } = renderHook(() => useSearch(VIEW.NAME, ITEM_COLUMNS_NAME.NAME));
+    const search = result.current;
 
     it('is expected to return defined', () => {
-      expect(result.current).toBeDefined();
+      expect(search).toBeDefined();
+    });
+
+    it('isDefaultState() returns true', () => {
+      expect(search.isDefaultState()).toEqual(true);
     });
   });
 });
