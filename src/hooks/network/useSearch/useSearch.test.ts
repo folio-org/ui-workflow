f10fbccc89a2a363afc475ff075b8312234aa1ad
Kevin Day <kday@tamu.edu>
2024-11-07 09:22:15 -0600

    Remove unused import.
    This import was being used for other tests that had too many problems that they were removed.
    The problems are related to Jest being too fickle.
    
    I forgot to remove this import.

--- src/hooks/network/useSearch/useSearch.test.ts
+++ src/hooks/network/useSearch/useSearch.test.ts
@@ -1,5 +1,4 @@
 import { renderHook } from '@testing-library/react';
-import { SearchField } from '@folio/stripes/components';
 
 import { ITEM_COLUMNS_NAME, VIEW } from '../../../constants';
 import { useSearch } from '.';
